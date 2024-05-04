1278cc5b91abf51c29a64c7512f2024fc325a6d4
Aryan Dande <86364775+Aryan-code-dev@users.noreply.github.com>
2024-05-04 19:06:22 +0530

    changed connection string

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,22 @@
+// require("dotenv").config();
+
+// const mongoose = require("mongoose");
+// const DB_NAME = process.env.DB_NAME;
+
+
+
+// mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`
+// ).then(() => console.log("Mongo DB Connected")
+// ).catch((err) => console.log("Mongo DB Error:", err));
+
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-const DB_NAME = process.env.DB_NAME;
+const password = process.env.password;
+const uname = process.env.uname
 
 
 
-mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`
-).then(() => console.log("Mongo DB Connected")
-).catch((err) => console.log("Mongo DB Error:", err));
+const con_string = 'mongodb+srv://'+uname+':'+password+'@cluster0.dnwerzh.mongodb.net/capitree?retryWrites=true&w=majority';
 
+mongoose.connect(con_string);
